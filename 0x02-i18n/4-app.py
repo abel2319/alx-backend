798207f3f2f7f72c2c2a611598ba451a5d97d142
abel2319 <abelbakary@gmail.com>
2023-11-01 21:29:06 +0100

    feat(task4):task 4 ✅

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""0. Basic Flask app
+"""
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for Flask
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determine the best match with our supported languages
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def hello_world() -> str:
+    """Hello Holberton
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
