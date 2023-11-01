bd0317930bf6cacfa0bc545db468189a2158c959
abel2319 <abelbakary@gmail.com>
2023-11-01 20:49:42 +0100

    fix(task2): task2 🩹 1.2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,6 +22,8 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """determine the best match with our supported languages
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
