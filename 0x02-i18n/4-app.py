4cdd974fcea1e30c37d599455ba1d5abd6313f34
abel2319 <abelbakary@gmail.com>
2023-11-01 23:09:33 +0100

    fix(task5): task5 🩹 1.1

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -36,7 +36,7 @@ def get_locale() -> str:
 def hello_world() -> str:
     """Hello Holberton
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
