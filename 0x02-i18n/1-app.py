e91f3ae5f6c05bb2ef5df3298887b3814d3a50db
abel2319 <abelbakary@gmail.com>
2023-11-01 19:07:21 +0100

    feat(task1):task 1 ✅

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""0. Basic Flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for Flask
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def hello_world():
+    """Hello Holberton
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
